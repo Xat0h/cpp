f3ca7d898cdbcd541f004e89d30b31416405577f
Xat0h <115512678+Xat0h@users.noreply.github.com>
2023-04-25 06:40:56 +1000

    Tutorial Projects
    Basic C++

---
+++ tutorial1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void get_emergency_number(std::string emergency_number) {
+	std::cout << "Dial " << emergency_number;
+}
+
+int main() {
+	std::string old_emergency_number = "999";
+	std::string new_emergency_number = "0118 999 881 999 119 725 3";
+
+	get_emergency_number(new_emergency_number);
+}
+
+void printFunction() {
+
+}
---
+++ tutorial2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// Define needs_water() here:
+std::string needs_water(int days, bool is_succulent) {
+
+    if (days > 3 && is_succulent == false) {
+        return "Time to water the plant.";
+    }
+    else if (days < 13 && is_succulent) {
+        return "Don't water the plant!";
+    }
+    else if (days >= 13 && is_succulent) {
+        return "Go ahead and give the plant a little water.";
+    }
+    else {
+        return "Don't water the plant!";
+    }
+
+}
+
+int main() {
+
+    std::cout << needs_water(10, false) << "\n";
+
+}
