bdaf7b933b74fc4e02ccd85a070804a39dffe756
Xat0h <115512678+Xat0h@users.noreply.github.com>
2023-05-02 04:50:39 +1000

    Tutorial - Namespaces

---
+++ namespaces.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using std::string; 
+using std::cout;
+using std::endl;
+
+int main() {
+	cout << "Hello World" << endl;
+	string s{ "How to use std without the entire standard namespace" };
+	cout << s << endl;
+}
