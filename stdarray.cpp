b1a58e04c3984a850ae81a325dc3122547244ac5
Xat0h <115512678+Xat0h@users.noreply.github.com>
2023-05-02 15:37:33 +1000

    WIP - Standard Array

---
+++ stdarray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using data = std::vector<int>;
+using std::find_if;
+using size = std::size_t;
+using std::array;
+
+
+template<size_t size>
+
+void better_f(array<int, size>& arr) {}
+
+void using_c_array() {
+    const int n = 256;
+    array<int, n> arr{};
+
+    better_f(arr);
+}
+
+int main() {
+    cout << "Hello world!" << "\n";
+
+    return 0;
+}
